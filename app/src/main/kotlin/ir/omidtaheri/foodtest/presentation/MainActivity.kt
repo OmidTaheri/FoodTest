a753a133f5a81137f758095ee9d3de19bffa97e5
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-10-22 23:22:30 +0330

    Add navigation Drawer to MainActivity

--- app/src/main/kotlin/ir/omidtaheri/foodtest/presentation/MainActivity.kt
+++ app/src/main/kotlin/ir/omidtaheri/foodtest/presentation/MainActivity.kt
@@ -1,12 +1,29 @@
 package ir.omidtaheri.foodtest.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import ir.omidtaheri.foodtest.R
+import ir.omidtaheri.foodtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration =
+            AppBarConfiguration(navController.graph, binding.drawerLayout)
+        binding.navigationView.setupWithNavController(navController)
+
     }
 }

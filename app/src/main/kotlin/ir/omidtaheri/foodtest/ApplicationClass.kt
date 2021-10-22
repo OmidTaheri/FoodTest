81cf78f7d846b539c154ffb840913f6c80a79231
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-10-22 20:54:27 +0330

    Add Application class to app module

---
+++ app/src/main/kotlin/ir/omidtaheri/foodtest/ApplicationClass.kt
@@ -0,0 +1,28 @@
+package ir.omidtaheri.foodtest
+
+import android.app.Application
+import ir.omidtaheri.foodtest.di.ApplicationComponentProvider
+import ir.omidtaheri.foodtest.di.components.ApplicationComponent
+import ir.omidtaheri.foodtest.di.components.DaggerApplicationComponent
+
+class ApplicationClass : Application(), ApplicationComponentProvider {
+
+    private lateinit var applicationComponent: ApplicationComponent
+
+    override fun onCreate() {
+        super.onCreate()
+
+        this.applicationComponent =
+            DaggerApplicationComponent.factory()
+                .create(this, "FoodDatabase")
+
+        applicationComponent.inject(this)
+
+    }
+
+    override fun provideApplicationComponent(): ApplicationComponent {
+        return applicationComponent
+    }
+
+
+}

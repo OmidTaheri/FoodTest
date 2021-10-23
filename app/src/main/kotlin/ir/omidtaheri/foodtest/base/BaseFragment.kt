1ef4ddcf22a2d8eabb059b735c5acd1bd15aea03
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-10-23 12:07:13 +0330

    Add BaseFragment in base package (basic requirements in fragments)

---
+++ app/src/main/kotlin/ir/omidtaheri/foodtest/base/BaseFragment.kt
@@ -0,0 +1,40 @@
+package ir.omidtaheri.foodtest.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import ir.omidtaheri.foodtest.base.viewmodelutils.ViewModelAssistedFactory
+import javax.inject.Inject
+
+abstract class BaseFragment<T : ViewModel> : Fragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelAssistedFactory<T>
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflateViewBinding(inflater, container)
+    }
+
+    abstract fun inflateViewBinding(inflater: LayoutInflater, container: ViewGroup?): View?
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindUi()
+        configDaggerComponent()
+        setLivaDataObservers()
+    }
+
+    abstract fun bindUi()
+
+    abstract fun setLivaDataObservers()
+
+    abstract fun configDaggerComponent()
+
+}

05c21a9f1ef2013a6d72495b9529b95c10f8fe2b
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-10-24 11:10:10 +0330

    Add BaseAndroidViewModel class (basic for any ViewModel)

---
+++ app/src/main/kotlin/ir/omidtaheri/foodtest/base/BaseAndroidViewModel.kt
@@ -0,0 +1,40 @@
+package ir.omidtaheri.foodtest.base
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+
+open class BaseAndroidViewModel(
+    private val mApplication: Application,
+    private val state: SavedStateHandle
+) : AndroidViewModel(mApplication) {
+
+    protected val compositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    protected val _isLoading: MutableLiveData<Boolean> = MutableLiveData()
+    val isLoading: LiveData<Boolean>
+        get() = _isLoading
+
+    fun addDisposable(disposable: Disposable) {
+
+        compositeDisposable.add(disposable)
+    }
+
+    fun removeDisposable(disposable: Disposable) {
+
+        compositeDisposable.delete(disposable)
+    }
+
+
+    private fun clearDisposables() {
+        compositeDisposable.clear()
+    }
+
+    override fun onCleared() {
+        clearDisposables()
+    }
+}

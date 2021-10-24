f851280edf013d1190f6b2cbbeca0dc07f56f374
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-10-24 13:49:46 +0330

    Remove unused dependencies

--- buildSrc/src/main/kotlin/dependencies/DependenciesVersions.kt
+++ buildSrc/src/main/kotlin/dependencies/DependenciesVersions.kt
@@ -17,25 +17,15 @@ object DependenciesVersions {
 
     object JetpackDependenciesVersions {
         const val CORE_KTX = "1.6.0"
-        const val FRAGMENT_KTX = "1.3.6"
-        const val LIVEDATA_KTX = "2.2.0"
-        const val LIFECYCLE = "2.3.1"
         const val ROOM = "2.3.0"
         const val NAVIGATION = "2.3.5"
-        const val PAGING = "3.0.1"
     }
 
 
     object UiDependenciesVersions {
         const val APPCOMPAT = "1.3.1"
         const val MATERIAL = "1.4.0"
-        const val RECYCLE_VIEW = "1.2.1"
         const val CONSTRAINT_LAYOUT = "2.1.1"
-        const val CARDVIEW = "1.0.0"
-        const val VIEWPAGER2 = "1.0.0"
-        const val VECTORE_DRAWABLE = "1.1.0"
-        const val COORDINATOR_LAYOUT = "1.1.0"
-        const val ANIMATED_VECTORE_DRAWABLE = "1.1.0"
     }
 
     object TestDependenciesVersions {
--- buildSrc/src/main/kotlin/dependencies/UiDependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/UiDependencies.kt
@@ -7,27 +7,11 @@ object UiDependencies {
     const val MATERIAL =
         "com.google.android.material:material:${DependenciesVersions.UiDependenciesVersions.MATERIAL}"
 
-    const val RECYCLER_VIEW =
-        "androidx.recyclerview:recyclerview:${DependenciesVersions.UiDependenciesVersions.RECYCLE_VIEW}"
-
     const val CONSTRAINT_LAYOUT =
         "androidx.constraintlayout:constraintlayout:${DependenciesVersions.UiDependenciesVersions.CONSTRAINT_LAYOUT}"
-
-    const val CARDVIEW =
-        "androidx.cardview:cardview:${DependenciesVersions.UiDependenciesVersions.CARDVIEW}"
-
-    const val VECTOR_DRAWABLE =
-        "androidx.vectordrawable:vectordrawable:${DependenciesVersions.UiDependenciesVersions.VECTORE_DRAWABLE}"
-
-    const val ANIMATED_VECTOR_DRAWABLE =
-        "androidx.vectordrawable:vectordrawable-animated:${DependenciesVersions.UiDependenciesVersions.ANIMATED_VECTORE_DRAWABLE}"
 
     const val GLIDE =
         "com.github.bumptech.glide:glide:${DependenciesVersions.GeneralDependenciesVersions.GLIDE}"
 
-    const val VIEWPAGER2 =
-        "androidx.viewpager2:viewpager2:${DependenciesVersions.UiDependenciesVersions.VIEWPAGER2}"
 
-    const val COORDINATOR_LAYOUT =
-        "androidx.coordinatorlayout:coordinatorlayout:${DependenciesVersions.UiDependenciesVersions.COORDINATOR_LAYOUT}"
 }

4be95df400b8c0436b16201d19d5709c60a6ee51
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-11-01 13:12:40 +0330

    Increment version code and version name to 1.0.3

--- buildSrc/src/main/kotlin/BuildAndroidConfig.kt
+++ buildSrc/src/main/kotlin/BuildAndroidConfig.kt
@@ -4,8 +4,8 @@ object BuildAndroidConfig {
     const val COMPILE_SDK_VERSION = 30
     const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 30
-    const val VERSION_CODE = 2
-    const val VERSION_NAME = "1.0.1"
+    const val VERSION_CODE = 3
+    const val VERSION_NAME = "1.0.2"
     const val SUPPORT_LIBRARY_VECTOR_DRAWABLES = true
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 }
